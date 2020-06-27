291e675cf60f522e68cecf45450d7c96a29ebe63
Robin Meischke <robin@nierhain.de>
2020-06-27 12:31:21 +0200

    branch prep

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.svg" >
+    <Portfolio />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Portfolio from './components/Portfolio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Portfolio
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #272D2D;
+  color: #F6F8FF;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Portfolio</h2>
+        <div v-for="project in projects" :key="project.title">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Portfolio",
+    data: function() {
+        return {
+            projects : []
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
