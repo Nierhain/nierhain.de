deb806cccca1f060c76ce04fa6bc31d58cd7ab6e
Robin Meischke <robin@nierhain.de>
2020-10-06 17:10:20 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+      }
+    },
+    extend: {
+      colors: {
+        background: '#282a36',
+        selection: '#44475a',
+        cyan: '#8be9fd',
+        green: '#50fa7b',
+        orange: '#ffb86c',
+        pink: '#ff79c6',
+        purple: '#bd93f9',
+        red: '#ff5555',
+        yellow: '#f1fa8c',
+        white: '#f8f8f2'
+      }
+    },
   },
   variants: {},
   plugins: [],
